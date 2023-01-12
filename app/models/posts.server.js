f4fb4dc068baf2100db34481943778ab8a4ae19c
DanteHarold <novianrelay@gmail.com>
2023-01-12 18:01:17 -0500

    deployment finished

--- app/models/posts.server.js
+++ app/models/posts.server.js
@@ -1,11 +1,11 @@
 export async function getPosts() {
-  const rpta = await fetch(` ${process.env.API_URL}/posts?populate=imagen`);
+  const rpta = await fetch(`${process.env.API_URL}/posts?populate=imagen`);
   const data = await rpta.json();
   return data;
 }
 export async function getPost(url) {
   const rpta = await fetch(
-    ` ${process.env.API_URL}/posts?filters[url]=${url}&populate=imagen`
+    `${process.env.API_URL}/posts?filters[url]=${url}&populate=imagen`
   );
   const data = await rpta.json();
   return data;
